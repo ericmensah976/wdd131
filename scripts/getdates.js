c6d2ee1e96a91315f1831a7da84fe4a910618445
ericmensah976 <emen2140@yahoo.com>
2025-05-12 15:32:53 +0100

    webpage

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Populate current year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Populate last modified date
+document.getElementById("lastModified").textContent = 
+  "Last Modification: " + document.lastModified;
